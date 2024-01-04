648cf6c0ebecba83d4721c7bc6ae64933133e935
admin <scehovdaniil@gmail.com>
2024-01-04 21:08:52 +0300

    added a new func and docs her and docs for first func

--- str_func.py
+++ str_func.py
@@ -1,5 +1,4 @@
 def capital_letters():
-<<<<<<< HEAD
     """Делает загланые все буквы каждого каждого слова в строке"""
     str_input = input()
     return str_input.upper()
@@ -10,8 +9,7 @@ def capitalized_first_letter():
     split_letter = letter_input.split()
     capilize_letter = split_letter.capitalize()
     return ''.join(capilize_letter)
-=======
+
     """Принимает на вход строку и возвращает её со всеми заглавными буквами"""
     str_input = input()
     return str_input.upper()
->>>>>>> develop
